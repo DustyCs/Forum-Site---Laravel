ce60d1eebce320b0241fbea3ebee1dca9606479e
DustyC <123617229+DustyCs@users.noreply.github.com>
2024-08-10 22:35:37 +0800

    Update: Made changes on route web.php and created forum controller trying to learn how to use 'x-layouts' using Components

--- ForumSite/routes/web.php
+++ ForumSite/routes/web.php
@@ -1,12 +1,10 @@
 <?php
 
+use App\Http\Controllers\ForumController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-
-Route::get('/Forum', function () {
-    return view('welcome');
-});
+Route::resource('/Forum', ForumController::class);
